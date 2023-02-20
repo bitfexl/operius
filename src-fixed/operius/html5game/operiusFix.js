96566b169b9cfc6444a637d704cc56108f9f6cbb
bitfexl <54662051+bitfexl@users.noreply.github.com>
2023-02-20 11:28:16 +0100

    Update operiusFix.js

--- src-fixed/operius/html5game/operiusFix.js
+++ src-fixed/operius/html5game/operiusFix.js
@@ -4,4 +4,7 @@ let body = document.getElementsByTagName("body")[0];
 
 body.onclick = function () {
   body.removeChild(enableAudio);
+  body.addEventListener("click", (e) => {
+    e.stopPropagation();
+  }, true);
 };
